8cbf00e30518218b8207377980f3a23de0b8a76f
KENY <piotrstudniak@gmail.com>
2025-11-03 20:07:32 +0100

    Add screen refresh on window resize

---
+++ JS/Screen refresh after window resize.js
@@ -0,0 +1,16 @@
+// Screen refresh after window resize
+
+jQuery(function ($) {
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+
+  $(window).resize(function () {
+    if (
+      windowWidth != $(window).width() ||
+      windowHeight != $(window).height()
+    ) {
+      location.reload();
+      return;
+    }
+  });
+});
