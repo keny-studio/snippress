f36973ac6b862566624a27b8448e8213e01a7a4e
KENY <piotrstudniak@gmail.com>
2026-05-15 08:19:26 +0200

    Disable auto-update email notifications for plugins and themes
    Added filters to disable email notifications for auto-updates of plugins and themes.

---
+++ PHP/functions php/Disable auto-updates email notification.php
@@ -0,0 +1,7 @@
+<?php
+
+// Disable plugins auto-update email notifications .
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+ 
+// Disable themes auto-update email notifications.
+add_filter( 'auto_theme_update_send_email', '__return_false' );
